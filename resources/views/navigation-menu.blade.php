64cee22ac166c2fdb13be176199d182dad834bf1
Leandro <leandroemmanuel_99@outlook.es>
2023-10-17 16:57:29 -0300

    Actualizado con código de main

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,12 +1,10 @@
-<div class="relative bg-white">
+<div class="relative bg-indigo-600">
     <!-- Contenido del encabezado -->
-    <header class="container mx-auto px-4 py-1 flex items-center">
-        <img src="{{ asset('images/LogoIvess-2.gif') }}" alt="Logo Ivess" class="mr-2 border-r-2 border-indigo-600 pr-4 -mt-3">
+    <header class="container mx-auto px-4 py-1 flex justify-between items-center">
+        <a href="#" onclick="location.reload(); return false;">
+            <h1 class="text-3xl font-semibold text-white">Vortex-Data (Procesador de Archivos)</h1>
+        </a>
 
-        <div class="ml-2">
-            <a href="#" onclick="location.reload(); return false;" class="no-underline">
-                <h1 class="text-3xl font-semibold text-indigo-600 pl-16">Vortex-Data (Procesador de Archivos)</h1>
-            </a>
-        </div>
+        <img src="{{ asset('images/LogoIvess-2.gif') }}" alt="Logo Ivess" class="ml-auto">
     </header>
 </div>
